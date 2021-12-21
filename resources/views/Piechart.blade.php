0ead6987b3bef2553073125fbe8f16fe22313b7a
Manju-Mishra <90688601+Manju-Mishra@users.noreply.github.com>
2021-12-22 02:38:11 +0530

    Update Piechart.blade.php

--- resources/views/Piechart.blade.php
+++ resources/views/Piechart.blade.php
@@ -2,16 +2,25 @@
 <!DOCTYPE html>
 <html>
   <head>
+  
+  <!--Load the AJAX API-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <style>
-      
-    </style>
     <script type="text/javascript">
+    
+    
+      // Load the Visualization API and the piechart package.
       google.charts.load('current', {'packages':['corechart']});
+      
+      // Set a callback to run when the Google Visualization API is loaded.
       google.charts.setOnLoadCallback(drawChart);
-
+      
+      
+       // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
       function drawChart() {
-
+         
+         // Create our data table.
         var data = google.visualization.arrayToDataTable([
           ['Task', 'Hours per Day'],
            <?php echo $chartData?>
@@ -21,7 +30,9 @@
           title: 'Products Available',
           is3D:true
         };
-
+         
+         
+          // Instantiate and draw our chart, passing in some options.
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
         chart.draw(data, options);
